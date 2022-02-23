b1e2e8df99135589d6bdbb4d6874879ac9324b09
Favour Afolayan <favourafolayan@gmail.com>
2022-02-23 11:12:50 +0100

    :white_check_mark: add to snake case function

--- src/to-snake-case/to-snake-case.test.ts
+++ src/to-snake-case/to-snake-case.test.ts
@@ -3,9 +3,9 @@ import { toSnakeCase } from './to-snake-case';
 describe('Snake case', () => {
   it('converts word to snake case', () => {
     const phrase = 'I should be in snake case';
-    const phraseInSnakeCase = 'i should be in snake case';
+    const phraseInSnakeCase = 'i_should_be_in_snake_case';
 
-    const snakeCaseWord = toSnakeCase(phrase);
+    const snakeCaseWord = toSnakeCase(phrase, '_');
 
     expect(snakeCaseWord).toBe(phraseInSnakeCase);
   });
--- src/to-snake-case/to-snake-case.ts
+++ src/to-snake-case/to-snake-case.ts
@@ -1,1 +1,12 @@
-export const toSnakeCase = (phrase: string): string => phrase.toUpperCase();
+export const toSnakeCase = (phrase: string, delimiter = '-'): string => {
+  // convert to lowercases first
+  const lowerCasedWords = phrase.toLowerCase();
+
+  // split the string into an array of words
+  const words = lowerCasedWords.split(' ');
+
+  // separate the words by underscores or whatever the separator is passed by the user
+
+  // return the joined words
+  return words.join(delimiter);
+};
